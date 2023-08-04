974d9a8f9499cbe4779cd5dfb505e4c1d1ed3d01
fferdialaca <75328943+ferdialaca@users.noreply.github.com>
2023-08-04 15:39:55 +0300

    ders4 ile class yapısı anlaşıldı

--- Ders4/class.dart
+++ Ders4/class.dart
@@ -1,62 +1,35 @@
+import 'customer.dart';
 void main() {
-  customer cust = customer("ferdi",150,3,'Ankara');
+  customer cust = customer(customerId:"2","ferdi",150,3,city: 'Ankara', customerGender: "Male");
   print(cust.getMoney());
-  int? money;
-  if(money != null)
-    print(money + 50);
-  else
+  if(cust.getMoney() != null){
+    cust.setMoney(cust.money+50);
+    print(cust.money);
+    }
+  else{
     print("hasikome");
-  
-  List<int?> moneys = [100, null, 0];
-  for(var i in moneys){
-    if(i != null){
-      if(i>0){
-        print("beyefendi");
-      }
-      else{
-        print("bye");
-      }
+  }
+  print("---"*10);
+  print(cust.isEmptyId);
+  if(cust.isSpecialCustomer("2")==true){
+    cust.money+=10;
+    print("tebrikler paranız 10 lira artıp ${cust.money} oldu.");
+  }
+  else{
+    print("hay aksi şeytan");
+  }
+
+  print("-----------"*5);
+  customer cust1 = customer(customerId:"1","ahmet", 100, 23);
+  print(cust1.name);
+  if(cust1.city == null){
+    print("${cust1.name} isimli müşteri şehir bilgisi vermemiş");
+  }
+  else {
+    if(cust1.city!.compareTo("Ankara") == 0){
+      print("tebrikler kazandınız");
     }
-    else{
-      print("hadi hesap açalım");
-    }
   }
-}
-class customer{
-  late String name;
-  late int money;
-  late int age;
-  late String city;
-  //constructor
-  customer(String name, int money, int age, String city){
-    this.name = name;
-    this.money = money;
-    this.age = age;
-    this.city = city;
-  }
-  //setters getters
-  void setName(String name){
-    this.name = name;
-  }
-  String getName(){
-    return this.name;
-  }
-  void setMoney(int money){
-    this.money = money;
-  }
-  int getMoney(){
-    return this.money;
-  }
-  void setAge(int age){
-    this.age = age;
-  }
-  int getAge(){
-    return this.age;
-  }
-  void setCity(String city){
-    this.city = city;
-  }
-  String getCity(){
-    return this.city;
-  }
+  print(cust1.userCode);
+  print(cust.toString());
 }
---
+++ Ders4/customer.dart
@@ -0,0 +1,73 @@
+class customer{
+  late final String _customerId;
+  late String name;
+  late int money;
+  late int age;
+  late String? city;
+  late String? customerGender;
+  late final String userCode;
+
+  //constructor
+  customer(this.name, this.money, this.age, {required String customerId, this.city, this.customerGender}){
+    _customerId = customerId;
+    userCode = (city ?? 'ist') + name;
+  }
+  
+  
+  
+  
+  
+  
+  //dart setter getter
+  String get getUserCode => userCode;
+  set setUserCode(String userCode) => this.userCode;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // classic setters getters
+  void setName(String name){
+    this.name = name;
+  }
+  String getName(){
+    return this.name;
+  }
+  void setMoney(int? money){
+    if(money == null || money < 0 ){
+      money = 0;
+    }
+    else{
+      this.money = money;
+    }
+  }
+  int getMoney(){
+    return this.money;
+  }
+  void setAge(int age){
+    this.age = age;
+  }
+  int getAge(){
+    return this.age;
+  }
+  void setCity({String? city}){
+    this.city = city;
+  }
+  String? getCity(){
+    return this.city;
+  }
+  String getCustomerId(){
+    return this._customerId;
+  }
+  // bool isSpecialCustomer(String customerId){
+  //   return _customerId == customerId;
+  // }
+  bool isSpecialCustomer(String customerId) => _customerId == customerId;
+  bool get isEmptyId => _customerId.isEmpty;
+}
